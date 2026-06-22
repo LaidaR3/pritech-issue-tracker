97763670c323683f541dc53485f5f786ed74dd9f
Laida Rusinovci <149337343+LaidaR3@users.noreply.github.com>
2026-06-22 22:46:31 +0200

    Add database schema for issue tracker

--- database/migrations/2026_06_22_203143_create_comments_table.php
+++ database/migrations/2026_06_22_203143_create_comments_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
+    
     public function up(): void
     {
-        Schema::create('issue_tag', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('issue_id')->constrained()->cascadeOnDelete();
+            $table->string('author_name');
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('issue_tag');
+        Schema::dropIfExists('comments');
     }
 };
--- database/migrations/2026_06_22_203144_add_start_date_and_deadline_to_projects_table.php
+++ database/migrations/2026_06_22_203144_add_start_date_and_deadline_to_projects_table.php
@@ -4,25 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->date('start_date')->nullable();
+            $table->date('deadline')->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropColumn(['start_date', 'deadline']);
         });
     }
 };
