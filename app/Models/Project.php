f14bfc74a421febd4a279b677e714d1849ec61ea
Laida Rusinovci <149337343+LaidaR3@users.noreply.github.com>
2026-06-22 22:48:23 +0200

    Add model relationships

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'deadline',
+    ];
+
+    public function issues()
+    {
+        return $this->hasMany(Issue::class);
+    }
 }
